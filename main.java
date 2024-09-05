0189bb2f788e840662ff354636640a50fb271b21
hiephp2606 <hoanghiep.hp2606@gmail.com>
2024-09-05 08:45:36 +0700

    lan 1

---
+++ main.java
@@ -0,0 +1,10 @@
+import entities.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+    }
+}
---
+++ service/AccountService.java
@@ -0,0 +1,14 @@
+package service;
+
+import entities.Account;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class AccountService {
+    public Account accountRegister (Scanner scanner, List<Account> accounts, int numberAccount) {
+        for (int i = 0; i < numberAccount; i++) {
+
+        }
+    }
+}
